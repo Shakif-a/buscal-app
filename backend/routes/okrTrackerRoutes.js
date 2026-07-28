b08cc36688e6a05c324e97cdd83e96fdcac03d6e
matthewlindsay314 <matthewlindsay314@gmail.com>
2026-07-28 16:52:38 +1000

    feat: basic okr tracker routes

---
+++ backend/routes/okrTrackerRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createObjective
+} = require("../controllers/calendarController");
+
+const { protect } = require("../middleware/authMiddleware");
+
+router.post("/objectives", protect, createObjective);
+
+// Default route
+router.get("/", (req, res) => {
+  res.json({ message: "OKR Tracker API endpoint" });
+});
+
+module.exports = router;
