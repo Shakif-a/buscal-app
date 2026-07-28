c64f249fc4f5c310187d8d5b67c201fde494d55d
matthewlindsay314 <matthewlindsay314@gmail.com>
2026-07-28 16:59:10 +1000

    fix: module mounted

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use("/api/calendar", require("./routes/calendarRoutes"));
 app.use("/api/users", require("./routes/userRoutes")); // Auth + account settings
 
 // ── Add new module routes below this line ────────────────────────────────────
-// app.use("/api/okr", require("./routes/okrRoutes"));
+app.use("/api/okrTracker", require("./routes/okrTrackerRoutes"));
 
 // Error handling middleware (must be after all routes)
 app.use(errorHandler);
