0d0374963bfadbcc773cdf6540c23e07afd854e1
hamim <122157154+hamimabbu@users.noreply.github.com>
2026-08-05 17:16:00 +1000

    Restore backend server after merge

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,27 +3,42 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
 const protect = asyncHandler(async (req, res, next) => {
-  const header = req.headers.authorization;
+  let token;
 
-  if (!header || !header.startsWith("Bearer ")) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(" ")[1];
+
+      // console.log("token: ", token);
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      if (!req.user) {
+        console.log("NO USER FOUND");
+        throw new Error("User not found");
+      }
+
+      // console.log("req.user: ", req.user);
+      next();
+    } catch (error) {
+      console.log("Error in authMiddleware.js:");
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+
+  if (!token) {
     res.status(401);
     throw new Error("Not authorized, no token");
   }
-
-  try {
-    const token = header.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select("-password");
-
-    if (!req.user) {
-      throw new Error("User not found");
-    }
-  } catch (error) {
-    res.status(401);
-    throw new Error("Not authorized");
-  }
-
-  next();
 });
 
 module.exports = { protect };
