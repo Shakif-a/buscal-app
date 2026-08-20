fa7828ad1b40c48cdd48ec5356e36f222daa0bfe
hamim <122157154+hamimabbu@users.noreply.github.com>
2026-08-20 13:00:55 +1000

    Complete objective edit and delete backend

--- backend/controllers/okrController.js
+++ backend/controllers/okrController.js
@@ -1,7 +1,9 @@
 const asyncHandler = require("express-async-handler");
+const mongoose = require("mongoose");
 const OkrObjective = require("../models/okrObjectiveModel");
 const OkrKeyResult = require("../models/okrKeyResultModel");
 const CalendarEntry = require("../models/calendarEntryModel");
+const User = require("../models/userModel");
 
 function getName(user) {
   let name = "";
@@ -187,33 +189,108 @@ const updateObjective = asyncHandler(async (req, res) => {
     throw new Error("Objective not found");
   }
 
-  if (req.body.title) {
-    objective.title = req.body.title;
+  if (req.body.title !== undefined) {
+    if (typeof req.body.title !== "string") {
+      res.status(400);
+      throw new Error("Please add a valid title");
+    }
+
+    const title = req.body.title.trim();
+
+    if (!title) {
+      res.status(400);
+      throw new Error("Please add a title");
+    }
+
+    objective.title = title;
   }
 
-  if (req.body.description) {
+  if (req.body.description !== undefined) {
+    if (typeof req.body.description !== "string") {
+      res.status(400);
+      throw new Error("Please add a valid description");
+    }
+
     objective.description = req.body.description;
   }
 
-  if (req.body.group) {
-    objective.group = req.body.group;
+  if (req.body.group !== undefined) {
+    if (typeof req.body.group !== "string") {
+      res.status(400);
+      throw new Error("Please select a valid group");
+    }
+
+    const group = req.body.group.trim();
+
+    if (!group) {
+      res.status(400);
+      throw new Error("Please select a group");
+    }
+
+    objective.group = group;
   }
 
-  if (req.body.owner) {
+  if (req.body.owner !== undefined) {
+    if (!mongoose.isValidObjectId(req.body.owner)) {
+      res.status(400);
+      throw new Error("Please select a valid owner");
+    }
+
+    const ownerExists = await User.exists({ _id: req.body.owner });
+
+    if (!ownerExists) {
+      res.status(400);
+      throw new Error("Selected owner was not found");
+    }
+
     objective.owner = req.body.owner;
   }
 
-  if (req.body.dueDate) {
-    objective.dueDate = req.body.dueDate;
+  if (req.body.dueDate !== undefined) {
+    if (!req.body.dueDate) {
+      res.status(400);
+      throw new Error("Please add a valid due date");
+    }
+
+    const dueDate = new Date(req.body.dueDate);
+
+    if (isNaN(dueDate.getTime())) {
+      res.status(400);
+      throw new Error("Please add a valid due date");
+    }
+
+    objective.dueDate = dueDate;
   }
 
-  if (req.body.commitmentType) {
-    objective.commitmentType = req.body.commitmentType;
+  let type = req.body.commitmentType;
+
+  if (req.body.type !== undefined) {
+    type = req.body.type;
+  }
+
+  if (type !== undefined) {
+    if (typeof type !== "string") {
+      res.status(400);
+      throw new Error("Please select a valid objective type");
+    }
+
+    type = type.toLowerCase();
+
+    if (type !== "committed" && type !== "aspirational") {
+      res.status(400);
+      throw new Error("Please select a valid objective type");
+    }
+
+    objective.commitmentType = type;
   }
 
   await objective.save();
+  await objective.populate("owner", "firstName lastName");
 
-  res.status(200).json(objective);
+  const data = await loadObjective(objective);
+  data.objective.keyResults = data.keyResults;
+
+  res.status(200).json(data.objective);
 });
 
 const deleteObjective = asyncHandler(async (req, res) => {
